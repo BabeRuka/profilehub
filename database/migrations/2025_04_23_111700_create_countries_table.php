a919840b05e8105bb84f8d83dbadc4c8ed68fddd
Solomon Bareebe <baberuka@gmail.com>
2025-11-14 09:19:06 +0200

    Add files via upload

--- database/migrations/2025_04_23_111700_create_countries_table.php
+++ database/migrations/2025_04_23_111700_create_countries_table.php
@@ -11,15 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('countries', function (Blueprint $table) {
-            $table->integer('country_id')->primary();
-            $table->string('country_name')->nullable();
-            $table->text('country_desc')->nullable();
-            $table->string('country_code')->nullable();
-            $table->string('dialing_code')->nullable();
-            $table->dateTime('create_date')->nullable();
-            $table->timestamp('modified_date')->useCurrentOnUpdate()->nullable()->useCurrent();
-        });
+        if (! Schema::hasTable('countries')) {
+            Schema::create('countries', function (Blueprint $table) {
+                $table->integer('country_id')->primary();
+                $table->string('country_name')->nullable();
+                $table->text('country_desc')->nullable();
+                $table->string('country_code')->nullable();
+                $table->string('dialing_code')->nullable();
+                $table->dateTime('create_date')->nullable();
+                $table->timestamp('modified_date')->useCurrentOnUpdate()->nullable()->useCurrent();
+            });
+        }
     }
 
     /**
